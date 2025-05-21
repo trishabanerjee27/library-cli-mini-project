9e4d834b606bdfc343730524edd28c47e5066439
trishabanerjee27 <75391092+trishabanerjee27@users.noreply.github.com>
2025-05-21 02:34:50 -0700

    uploading project from local

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # library-cli-mini-project
+
+# CLI Library System
+
+A simple Python command-line library system with:
+
+- Add, list, lend, and return books
+- Data saved in `library_data.json`
+- Organized structure with code in `library.py` and CLI in `main.py`
+
+All book operations update a local JSON file for data persistence between sessions..
+
+## Repo Structure
+```
+library_cli_project/
+├── main.py
+├── library.py
+├── README.md
+└── data/
+    └── library_data.json
+```
+
+## Usage
+
+# Navigate to the project directory
+cd library_cli_project
+
+# Run the CLI application
+python main.py
+
--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+from library import Library
+
+def main():
+    library = Library()
+
+    while True:
+        print("\n~~~~~~~ LIBRARY MENU ~~~~~~~~")
+        print("1. Add Book")
+        print("2. List Books")
+        print("3. Borrow Book")
+        print("4. Return Book")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            title = input("Book Title: ")
+            author = input("Author: ")
+            isbn = input("ISBN: ")
+            library.add_book(title, author, isbn)
+
+        elif choice == "2":
+            library.list_books()
+
+        elif choice == "3":
+            isbn = input("Enter ISBN to borrow: ")
+            library.lend_book(isbn)
+
+        elif choice == "4":
+            isbn = input("Enter ISBN to return: ")
+            library.return_book(isbn)
+
+        elif choice == "5":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid option! Try again!")
+
+if __name__ == "__main__":
+    main()
 
